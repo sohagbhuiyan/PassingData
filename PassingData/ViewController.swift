16aec008599a16bb5b22cb459103229d2b8b2e67
sohagbhuiyan <smsohag171@gmail.com>
2025-02-12 00:35:08 +0600

    Initial Commit

---
+++ PassingData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PassingData
+//
+//  Created by macmini on 2/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
